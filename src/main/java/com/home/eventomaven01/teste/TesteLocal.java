9476437f54e9b5c1b8d632f02d9cf7c6295eb30f
hp <hp@hp-PC>
2023-02-23 14:20:22 -0300

    primeiro commit

---
+++ src/main/java/com/home/eventomaven01/teste/TesteLocal.java
@@ -0,0 +1,21 @@
+ 
+package com.home.eventomaven01.teste;
+
+import com.home.eventomaven01.dao.LocalDAO;
+import com.home.eventomaven01.model.Local;
+
+ 
+public class TesteLocal {
+    public static void main(String[] args) {
+        Local l = new Local();
+        l.setPredio("Alphaville");
+        l.setSala("J440");
+        l.setCapacidade(500);
+//        l.setId(1L);
+        
+        LocalDAO dao = new LocalDAO();
+        l=dao.Salvar(l);
+        
+        System.out.println("Local salvo "+l.getId()+" "+l.getPredio()+" "+l.getSala()+" "+l.getCapacidade());
+    }
+}
