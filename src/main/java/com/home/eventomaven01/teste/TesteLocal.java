72b1de6853ec71a9eed67ce73eb7e4f1e02de892
Sousa-Edson <>
2023-02-23 15:38:16 -0300

    começando a modificar os testes

--- src/main/java/com/home/eventomaven01/teste/TesteLocal.java
+++ src/main/java/com/home/eventomaven01/teste/TesteLocal.java
@@ -6,7 +6,7 @@ import com.home.eventomaven01.model.Local;
 
  
 public class TesteLocal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Local l = new Local();
         l.setPredio("Barueri");
         l.setSala("B1010");
