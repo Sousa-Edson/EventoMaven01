d6c417783300635ef938772f0d908fbce305d7ca
Sousa-Edson <hp@hp-PC>
2023-02-24 10:54:18 -0300

    funcinando tabela de associação

--- src/main/java/com/home/eventomaven01/teste/PalestraTeste.java
+++ src/main/java/com/home/eventomaven01/teste/PalestraTeste.java
@@ -10,23 +10,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PalestraTeste {
-
+    
     public static void main(String[] args) throws Exception {
         DateFormat df = new SimpleDateFormat("dd/MM/yyy HH:mm");
-
+        
         Palestrante pa = new Palestrante();
-        pa.setNome("Joao Pedro");
-        pa.setMiniBio("medico");
-
+//        pa.setNome("Joao Pedro");
+//        pa.setMiniBio("medico");
+        pa.setId(5L);
+        
         List<Palestrante> palestrantes = new ArrayList<>();
         palestrantes.add(pa);
-
+        
         Local l = new Local();
 //        l.setPredio("Alpha");
 //        l.setSala("A23");
 //        l.setCapacidade(60);
         l.setId(12L);
-
+        
         Palestra p = new Palestra();
         p.setTitulo("Consultas personalizadas com JPA");
         p.setLocal(l);
@@ -34,7 +35,7 @@ public class PalestraTeste {
         p.setDataHora(df.parse("27/09/2023 09:00"));
         
         p.setPalestrantes(palestrantes);
-        
+
 //        pa.setPalestra(p);
 //
         PalestraDAO dao = new PalestraDAO();
@@ -45,5 +46,5 @@ public class PalestraTeste {
 //        Palestra p2 = dao.consultaPorId(2L);
 //        System.out.println("Palestra " + p2.getTitulo() + " " + p2.getLocal().getPredio() + " " + p2.getLocal().getSala());
     }
-
+    
 }
