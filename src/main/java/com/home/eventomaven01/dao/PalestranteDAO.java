474806249c38285976a7467fac3c04c4ca771202
Edson Sousa <edson3711.es@gmail.com>
2023-02-24 20:38:31 -0300

    consulta nomeada em palestra - finalizado

--- src/main/java/com/home/eventomaven01/dao/PalestranteDAO.java
+++ src/main/java/com/home/eventomaven01/dao/PalestranteDAO.java
@@ -1,10 +1,12 @@
 package com.home.eventomaven01.dao;
 
-import com.home.eventomaven01.model.Palestra;
 import com.home.eventomaven01.model.Palestrante;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 public class PalestranteDAO {
 
@@ -47,4 +49,20 @@ public class PalestranteDAO {
         }
         return p;
     }
+
+    public List<Palestrante> consultaPorEvento(Long eventoId) {
+        EntityManager em = getEM();
+        List<Palestrante> palestrantes;
+        try {
+            Query q = em.createNamedQuery("Palestrante.consultaPorEvento");
+            q.setParameter("eventoId", eventoId);
+            palestrantes = q.getResultList();
+        } catch (Exception e) {
+            palestrantes = new ArrayList();
+        } finally {
+            em.close();
+        }
+        return palestrantes;
+    }
+
 }
