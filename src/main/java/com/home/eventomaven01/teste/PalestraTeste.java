e1220c4a1570914c12699b3220d1a3f7fc9b9ade
Edson Sousa <edson3711.es@gmail.com>
2023-02-24 08:03:30 -0300

    verifiacr erro

--- src/main/java/com/home/eventomaven01/teste/PalestraTeste.java
+++ src/main/java/com/home/eventomaven01/teste/PalestraTeste.java
@@ -3,31 +3,45 @@ package com.home.eventomaven01.teste;
 import com.home.eventomaven01.dao.PalestraDAO;
 import com.home.eventomaven01.model.Local;
 import com.home.eventomaven01.model.Palestra;
+import com.home.eventomaven01.model.Palestrante;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PalestraTeste {
 
     public static void main(String[] args) throws Exception {
         DateFormat df = new SimpleDateFormat("dd/MM/yyy HH:mm");
-//        Local l = new Local();
-//        l.setPredio("Delta");
-//        l.setSala("D123");
-//        l.setCapacidade(60);
-//
-//        Palestra p = new Palestra();
-//        p.setTitulo("Relacionamento com JPA");
-//        p.setLocal(l);
-//        p.setDuracao(100);
-//        p.setDataHora(df.parse("26/09/2023 19:00"));
+        
+        Palestrante pa = new Palestrante();
+        pa.setNome("David Gabriel");
+        pa.setMiniBio("estudante");
+        
+        List<Palestrante> palestrantes = new ArrayList<>();
+        palestrantes.add(pa);
+        
+        
+        Local l = new Local();
+        l.setPredio("Delta");
+        l.setSala("D123");
+        l.setCapacidade(60);
+
+        Palestra p = new Palestra();
+        p.setTitulo("Relacionamento com JPA");
+        p.setLocal(l);
+        p.setDuracao(100);
+        p.setDataHora(df.parse("26/09/2023 19:00"));
+        
+        p.setPalestrante(palestrantes);
 //
         PalestraDAO dao = new PalestraDAO();
-//        p = dao.salvar(p);
-//        System.out.println("Palestra " + p.getTitulo() + " Salva");
-//
+        p = dao.salvar(p);
+        System.out.println("Palestra " + p.getTitulo() + " Salva");
+
 //        dao.remover(p.getId());
-Palestra p2 = dao.consultaPorId(2L);
-        System.out.println("Palestra "+p2.getTitulo()+" "+p2.getLocal().getPredio()+" "+p2.getLocal().getSala());
+//        Palestra p2 = dao.consultaPorId(2L);
+//        System.out.println("Palestra " + p2.getTitulo() + " " + p2.getLocal().getPredio() + " " + p2.getLocal().getSala());
 
     }
 
