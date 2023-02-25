5dbe0f8858e282b0a1b9a4329aa211f3ca9a9190
Edson Sousa <edson3711.es@gmail.com>
2023-02-24 21:03:36 -0300

    implementando genericoDAO

--- src/main/java/com/home/eventomaven01/teste/LocalTeste.java
+++ src/main/java/com/home/eventomaven01/teste/LocalTeste.java
@@ -7,24 +7,23 @@ public class LocalTeste {
 
     public static void main(String[] args) throws Exception {
         Local l = new Local();
-        l.setPredio("São Paulo");
-        l.setSala("SP001");
+        l.setPredio("Butanta");
+        l.setSala("BUBU90");
         l.setCapacidade(30);
-        
+
         LocalDAO dao = new LocalDAO();
         l = dao.Salvar(l); // executa o insert
-        
+
         System.out.println("Local salvo " + l.getId() + " " + l.getPredio() + " " + l.getSala() + " " + l.getCapacidade());
-        
-        Local l2 = dao.consultaPorId(l.getId()); // executa o update
+
+        Local l2 = dao.consultaPorId(Local.class, l.getId()); // executa o update
         System.out.println("Consultado o local " + l.getId() + " " + l.getCapacidade());
         l2.setCapacidade(120);
         l2 = dao.Salvar(l2);
-        
+
         System.out.println("Local salvo " + l2.getId() + " " + l2.getPredio() + " " + l2.getSala() + " " + l2.getCapacidade());
-        
-        dao.remover(l2.getId()); // executa delete
-        System.out.println("O local foi deletado");
+
+//       S
     }
-    
+
 }
