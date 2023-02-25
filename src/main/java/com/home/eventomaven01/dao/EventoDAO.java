fd4edac11f16fd4c270ba1877b1008560e8e0a57
Edson Sousa <edson3711.es@gmail.com>
2023-02-24 21:06:31 -0300

    implementando genericoDAO 2

--- src/main/java/com/home/eventomaven01/dao/EventoDAO.java
+++ src/main/java/com/home/eventomaven01/dao/EventoDAO.java
@@ -4,52 +4,11 @@ import com.home.eventomaven01.model.Evento;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.Query;
 
-public class EventoDAO {
+public class EventoDAO extends GenericoDAO<Evento> {
 
-    public EntityManager getEM() {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("dev-up");
-        return factory.createEntityManager();
-    }
-
-    public Evento salvar(Evento evento) throws Exception {
-        EntityManager em = getEM();
-        try {
-            em.getTransaction().begin();
-            em.persist(evento); // executa insert
-            em.getTransaction().commit();
-        } finally {
-            em.close();
-        }
-        return evento;
-    }
-
-    public void remover(Long id) {
-        EntityManager em = getEM();
-        try {
-            em.getTransaction().begin();
-            Evento e = em.find(Evento.class, id);
-            em.remove(e);
-            em.getTransaction().commit();
-        } finally {
-            em.close();
-        }
-    }
-
-    public Evento consultaPorId(Long id) {
-        EntityManager em = getEM();
-        Evento e = null;
-        try {
-            e = em.find(Evento.class, id);
-        } finally {
-            em.close();
-        }
-        return e;
-    }
-
+    
     public List<Evento> consultarTodos() {
         EntityManager em = getEM();
         List<Evento> eventos;
